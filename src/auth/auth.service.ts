80463ae7e00b6c909a29caa390d18d74e447f0c9
lucile <developer.lvt@gmail.com>
2023-12-05 14:41:28 +0100

    fixed prettier problems

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-    signup() {
-        return 'I am signed up'
-    }
+  signup() {
+    return 'I am signed up';
+  }
 
-    signin() {
-        return 'I am signed in'
-    }
+  signin() {
+    return 'I am signed in';
+  }
 }
